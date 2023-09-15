b8aa9f5b40248b50be8c2a59b2ae5944f5543524
Gaurav Pandey <yednapg@gmail.com>
2023-09-15 08:37:07 +0530

    added a.cpp

---
+++ a.cpp
@@ -0,0 +1,21 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+	// write from here
+//	}
+
+// use this command to run
+// g++ file_name.cpp -o file_name
+	
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	string g;
+	cin >> g;
+	cout << "Entered value is: "<< g;
+
+	return 0;
+}
