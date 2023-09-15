15cee06295d70dfcfb6a44993633deb1377b2115
Gaurav Pandey <yednapg@gmail.com>
2023-09-15 12:15:55 +0530

    added b

---
+++ b.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int k=x+y;
+	    if(k>6){cout<<"YES"<<endl;}
+	    else{cout<<"NO"<<endl;}
+	}
+	return 0;
+}
+
